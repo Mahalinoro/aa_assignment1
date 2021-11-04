c6f7befd7a18bd4fb199e4a16a8a80b988cd673d
Mahalinoro Razafimanjato <m.razafiman@alustudent.com>
2021-11-04 19:58:27 +0300

    updated files

--- main.py
+++ main.py
@@ -3,24 +3,16 @@
 # int ar[n] represents the colors of each sock within the array ar
 
 def sockMerchant(n, ar):
-    # This dictionary will store the number of stocks with the same color
-    socks_per_number = {}
     # int total_pairs will store the final number of pairs with matching colors
     total_pairs = 0
-    # Loop through the elements of array ar
-    for sock_color in range(len(ar)):
-        # Check if the sock color is not in number_of_socks yet
-        # If not, add it to the dictionary as the key and its value to 1
-        # Otherwise, increment the value of the sock color with 1
-        if ar[sock_color] not in socks_per_number:
-            socks_per_number[ar[sock_color]] = 1
-        else:
-            socks_per_number[ar[sock_color]] += 1
-    # Loop through the key, value pairs in the dictionary number of socks
-    for sock in socks_per_number:
-        # For each sock color and the total number, perform a floor division by 2
+    
+    # Loop through the elements of set ar 
+    # Using set to remove duplicates and remain with only the unique colors
+    for i in set(ar):
+        # For each sock color and its total number got from .count method, perform a floor division by 2
         # Then, add the result to total_pairs
-        total_pairs += socks_per_number[sock] // 2
+        total_pairs += ar.count(i) // 2        
+    
     # Return the final result with all the matching colors
     return total_pairs
 
--- test.py
+++ test.py
@@ -2,19 +2,15 @@ from main import sockMerchant
 import unittest
 
 class Test(unittest.TestCase):
-    def test_case_one(self):
+    def test_30(self):
         ar = [10, 20, 20, 10, 10, 30, 50, 10, 20, 30, 10, 20, 20, 10, 10, 30, 50, 10, 20, 30, 5, 8, 4, 15, 40, 60, 100, 42, 78, 54]
-        n = 30
-        result = 10
-        self.assertEqual(sockMerchant(n, ar), result)
+        self.assertEqual(sockMerchant(30, ar), 10)
 
-    def test_case_two(self):
+    def test_80(self):
         ar = [42, 42, 42, 42, 42, 42, 42, 42, 18, 42, 42, 42, 42, 42, 42, 42, 16, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42,
          42, 42, 42, 42, 42, 42, 90, 42, 42, 42, 20, 42, 85, 42, 42, 42, 42, 42, 40, 42, 42, 10, 42, 42, 20, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42,
           42, 42, 32, 42, 42, 42, 42]
-        n = 80
-        result = 36
-        self.assertEqual(sockMerchant(n, ar), result)
+        self.assertEqual(sockMerchant(80, ar), 36)
 
 if __name__ == '__main__':
     unittest.main()
